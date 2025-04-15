7dea2a078d63483a9363508970b63578527d1cd1
atefe fazeli <atefefazeli1996@outlook.com>
2025-04-15 20:22:26 +0330

    main structure completed

--- components/modules/navbar/navbar.js
+++ components/modules/navbar/navbar.js
@@ -1,19 +1,33 @@
 import Image from "next/image";
 import { useState } from "react";
 import { IoMenu } from "react-icons/io5";
+import { IoPersonCircleOutline } from "react-icons/io5";
 export default function Navbar() {
   const [showSidebar, setShowSidebar] = useState(false);
   function sidebarHandler() {
     setShowSidebar(!showSidebar);
   }
   return (
-    <div className="flex justify-between items-center h-10  md:h-12 bg-amber-200 w-full">
-      <button className="flex md:hidden" onClick={sidebarHandler}>
-        {" "}
-        <IoMenu />
-      </button>
-      <input className="hidden md:flex " placeholder="جست و جو کنید..."></input>
-      <Image className="rounded-4xl w-7 h-7" />
-    </div>
+    <>
+      <div className="flex justify-between items-center h-10  md:h-12 bg-amber-200 w-full relative px-6">
+        <button className="flex md:hidden z-50" onClick={sidebarHandler}>
+          {" "}
+          <IoMenu className="rounded-4xl w-8 h-8 text-green-700"/>
+        </button>
+        <input
+          className="hidden md:flex "
+          placeholder="جست و جو کنید..."
+        ></input>
+
+        <IoPersonCircleOutline className="rounded-4xl w-8 h-8 text-green-700" />
+      </div>
+      {showSidebar ? (
+        <div className="absolute top-0 left-0 w-full h-screen z-40 bg-neutral-400 pt-7">
+          fffffffffffffffffff
+        </div>
+      ) : (
+        ""
+      )}
+    </>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,13 @@ import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className="grid grid-cols-5">
-      <div className="hidden md:flex md:col-span-1 h-screen bg-amber-500"></div>
+    <div className="grid grid-cols-5 ">
+      <div className="hidden md:flex md:col-span-1 h-screen border-l border-l-gray-400 p-6"></div>
       <div className="col-span-5 md:col-span-4">
         <Navbar />
-        <Component {...pageProps} />
+        <div className="bg-pink-100 w-full overflow-auto p-6 h-[calc(100vh-2.5rem)]  md:h-[calc(100vh-3rem)]">
+          <Component {...pageProps} />
+        </div>
       </div>
     </div>
   );
