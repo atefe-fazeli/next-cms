5dfe3ed010427c64d83dd00c3579f0b393301792
atefe fazeli <atefefazeli1996@outlook.com>
2025-04-16 21:40:53 +0330

    course component completed

--- components/templates/index/courseItem.js
+++ components/templates/index/courseItem.js
@@ -1,3 +1,25 @@
+import Image from "next/image";
+import { FiEdit, FiTrash } from "react-icons/fi";
+
 export default function CourseItem() {
-  return <div className="flex items-center gap-3"></div>;
+  return (
+    <div className="flex items-center w-full md:w-10/12 shadow-2xl">
+      <div className="w-1/6">
+        <Image />
+      </div>
+      <div className="flex justify-between items-center p-3 w-5/6">
+        <h1 className="font-bold">دوره pwa</h1>
+        <div className="flex items-center gap-2">
+          <button className="bg-teal-400 px-5 py-3 rounded-md md:min-w-28">
+            <span className="text-white hidden md:flex"> ویرایش</span>
+            <FiEdit className="text-white flex md:hidden" />
+          </button>
+          <button className="bg-rose-800 px-5 py-3 rounded-md md:min-w-28">
+            <span className="text-white hidden md:flex"> حذف</span>
+            <FiTrash className="text-white flex md:hidden" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
+import CourseItem from "@/components/templates/index/courseItem";
+
 export default function Home() {
   return (
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col gap-3 md:gap-5">
       <div className="flex items-center justify-between w-full md:w-10/12">
         <h1 className="font-bold text-lg md:text-2xl text-gray-700 dark:text-gray-300">دوره ها</h1>
         <button className="px-10 py-2 rounded-md bg-green-400 ">افزودن دوره </button>
+        
       </div>
+      <div className="flex flex-col gap-2 md:gap-3"></div>
+      <CourseItem/>
     </div>
   );
 }
